91f7fb30663b317ca5e9fa37b874b5d45fcfec0b
Ajit singh <ajitkumarsingh999@gmail.com>
2017-06-30 20:07:34 +0530

    Add project files.

---
+++ DemoForms/DemoForms/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace DemoForms.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
