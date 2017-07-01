474d8bd27443e684a721b25794665fb7399879de
Ajit singh <ajitkumarsingh999@gmail.com>
2017-07-01 09:01:05 +0530

    Controls added to the form page, Countrulist json added.

---
+++ DemoForms/DemoForms/Models/Country.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+
+namespace DemoForms.Models
+{
+    public class Country
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        public static List<Country> GetCountryList()
+        {
+            var assembly = typeof(Country).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("DemoForms.Resources.CountryList.json");
+            string jsonString = "";
+            using (var reader = new StreamReader(stream))
+            {
+                jsonString = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<Country>>(jsonString);
+            }
+        }
+    }
+
+}
---
+++ DemoForms/DemoForms/ViewModels/FormDetailPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoForms.Models;
+
+namespace DemoForms.ViewModels
+{
+    public class FormDetailPageViewModel : BaseViewModel
+    {
+        private List<Country> countryList;
+
+        public List<Country> CountryList
+        {
+            get { return countryList; }
+            set { SetProperty(ref countryList, value); }
+        }
+
+        public FormDetailPageViewModel()
+        {
+            countryList = Country.GetCountryList();
+        }
+    }
+}
