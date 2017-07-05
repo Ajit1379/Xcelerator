94316b23e93d036b8e5478fdb056546313ba57a7
Ajit singh <ajitkumarsingh999@gmail.com>
2017-07-06 00:21:01 +0530

    added firebase realtime database link

--- DemoForms/DemoForms/Services/RestService.cs
+++ DemoForms/DemoForms/Services/RestService.cs
@@ -7,18 +7,42 @@ namespace DemoForms.Services
 {
     public class RestService
     {
+        public async Task<bool> LoadData()
+        {
+            using (var client = new HttpClient())
+            {
+                var restUrl = "https://xamarinfirebase-74fdd.firebaseio.com/.json";
+                var uri = new Uri(string.Format(restUrl, string.Empty));
+                try
+                {
+                    var response = await client.GetAsync(uri);
+                    response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+                return false;
+            }
+        }
+
         public async Task<bool> SendData(string json)
         {
             using (var client = new HttpClient())
             {
-                var restUrl = "http://api.myjson.com/bins/c4ucn";
+                var restUrl = "https://xamarinfirebase-74fdd.firebaseio.com/.json";
                 var uri = new Uri(string.Format(restUrl, string.Empty));
                 try
                 {
                     HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
                     HttpResponseMessage response = null;
-                    response = await client.PutAsync(uri, content);
+                    response = await client.PostAsync(uri, content);
                     if (response.IsSuccessStatusCode)
                     {
                         return true;
